7a423edbfacbf879b9d5a4dc32f2e8d1216b0d12
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-06-10 10:46:56 +0900

    update ESLint and AVA
    Highlight: ESLint v3-> v4 http://eslint.org/blog/2017/06/eslint-v4.0.0-released

--- test.js
+++ test.js
@@ -4,15 +4,13 @@ import allEslintRules from '.';
 import fetchCheerioObject from 'fetch-cheerio-object';
 
 test('The array includes all the ESLint rules except for the deprecated or removed ones.', async t => {
-  t.plan(1);
-
   const $ = await fetchCheerioObject('http://eslint.org/docs/rules/');
   const rulesOnWebsite = $('h2')
-  .eq(0)
-  .nextUntil('h2:contains("Deprecated")')
-  .find('td a')
-  .map((i, el) => $(el).attr('href'))
-  .get();
+    .eq(0)
+    .nextUntil('h2:contains("Deprecated")')
+    .find('td a')
+    .map((i, el) => $(el).attr('href'))
+    .get();
 
   t.deepEqual(difference(allEslintRules, rulesOnWebsite), []);
 });
