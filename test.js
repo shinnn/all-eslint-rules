ab3c3c654d5c2375da48419c0f253a1eeb886df4
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-22 19:43:58 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint form v4 to v5 https://eslint.org/docs/user-guide/migrating-to-5.0.0
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent

--- test.js
+++ test.js
@@ -4,13 +4,13 @@ import allEslintRules from '.';
 import fetchCheerioObject from 'fetch-cheerio-object';
 
 test('The array includes all the ESLint rules except for the deprecated or removed ones.', async t => {
-  const $ = await fetchCheerioObject('http://eslint.org/docs/rules/');
-  const rulesOnWebsite = $('h2')
-    .eq(0)
-    .nextUntil('#deprecated')
-    .find('td a')
-    .map((i, el) => $(el).attr('href'))
-    .get();
+	const $ = await fetchCheerioObject('http://eslint.org/docs/rules/');
+	const rulesOnWebsite = $('h2')
+	.eq(0)
+	.nextUntil('#deprecated')
+	.find('td a')
+	.map((i, el) => $(el).attr('href'))
+	.get();
 
-  t.deepEqual(difference(allEslintRules, rulesOnWebsite), []);
+	t.deepEqual(difference(allEslintRules, rulesOnWebsite), []);
 });
